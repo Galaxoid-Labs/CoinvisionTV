1934db0f1c19e55e58dbd67c2e1675fc58620070
Jacob <ismyhc@gmail.com>
2021-11-03 16:10:31 -0400

    Initial Commit

---
+++ Shared/CoinvisionTVApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinvisionTVApp.swift
+//  Shared
+//
+//  Created by Jacob Davis on 11/3/21.
+//
+
+import SwiftUI
+
+@main
+struct CoinvisionTVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
